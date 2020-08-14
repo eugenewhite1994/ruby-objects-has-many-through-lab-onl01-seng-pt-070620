4e8ac357458eeaec1f57d32bc6187bcbd9213972
Eugene White <eugenewhite1994@gmail.com>
2020-08-14 00:00:32 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,6 +1,7 @@
-class Genre 
+class Genre
 
-  attr_accessor :name, :song, :artist
+  attr_accessor :name
+
   @@all = []
 
   def initialize(name)
@@ -8,19 +9,18 @@ class Genre
     @@all << self
   end
 
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    self.songs.collect { |song| song.artist }
+  end
+
   def self.all
     @@all
   end
 
-  def new_song(name, artist)
-    Song.new(name, artist, self)
-  end
-
-  def songs
-    Song.all.select {|song| song.genre == self}
-  end
-
-  def artists
-    songs.collect {|song| song.artist}
-  end
-end 
+end
